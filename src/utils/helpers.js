07699a81c52f90c89d585e72690c36ffd0f1620d
Jhanvi Ramani <ramanijhanvi88@gmail.com>
2026-07-20 16:37:55 +0530

    Update project

--- src/utils/helpers.js
+++
@@ -1,6 +0,0 @@
-export const truncateText = (text, max = 100) => {
-  return text.length > max ? text.substring(0, max) + '...' : text;
-};
-
-export const capitalize = (text) =>
-  text.charAt(0).toUpperCase() + text.slice(1);
--- src/utils/validators.js
+++
@@ -1,8 +0,0 @@
-export const isEmail = (email) =>
-  /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-
-export const isPasswordStrong = (password) =>
-  password.length >= 8;
-
-export const isPhone = (phone) =>
-  /^[6-9]\d{9}$/.test(phone);
